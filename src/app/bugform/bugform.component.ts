fa14b05348c20df5a105c370acfdf888dc5cba3a
Harry Fytrakis <harry_fyt@hotmail.com>
2018-05-12 13:31:24 +0300

    my changes

--- src/app/bugform/bugform.component.ts
+++ src/app/bugform/bugform.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IBugdetails } from '../interfaces/bugdetails';
 import { Bugdetails } from '../classes/bugdetails';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { BugserviceService } from '../services/bugservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'brs-bugform',
@@ -16,30 +17,43 @@ export class BugformComponent implements OnInit {
   reporters = ['QA', 'PO', 'DEV'];
   statuses = ['Ready for test', 'Done', 'Rejected'];
   showValidations = false;
-  constructor(private bugservice: BugserviceService, ) {}
+
+  bugForm: FormGroup;
+
+  constructor(private bugservice: BugserviceService, private router: Router) {}
 
   ngOnInit() {
     this.model = new Bugdetails(null, null, null, null, null, null, null);
+
+    this.bugForm = new FormGroup({
+      title: new FormControl(this.model.title, Validators.required),
+      description: new FormControl(this.model.description, Validators.required),
+      priority: new FormControl(this.model.priority, Validators.required),
+      reporter: new FormControl(this.model.reporter, Validators.required),
+      status: new FormControl(this.model.status)
+    });
+
+    this.bugForm.get('reporter').valueChanges.subscribe(val => {
+      const statusFormControl = this.bugForm.controls.status;
+
+      if (val === 'QA') {
+        statusFormControl.setValidators(Validators.required);
+      } else {
+        statusFormControl.clearValidators();
+      }
+      statusFormControl.updateValueAndValidity();
+    });
+
   }
 
-  propertyIsValid(value, property) {
-    if (property === 'reporter' && value === 'QA') {
-      this.statusRequired = true;
-    } else if (property === 'status' && value == null) {
-      this.statusRequired = true;
-    } else {
-      this.statusRequired = false;
-    }
-  }
-
-  formSubmit (form: NgForm) {
-    console.log(form.value);
-    if (form.invalid) {
+  formSubmit ({value}: {value}) {
+    console.log(value);
+    if (this.bugForm.invalid) {
       this.showValidations = true;
       return;
     }
-    this.bugservice.createBug(form.value).subscribe();
-
+    this.bugservice.createBug(value).subscribe();
+    this.router.navigate(['./bugs']);
   }
 
 }
